aa7ff8fd5729caeada1474ce784b732b9f38c41c
OzerOzturk <ozerozturk1636@gmail.com>
2022-10-08 17:23:13 +0300

    feat(Category.jsx): add category component.

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import menuList from "../../assets/icons/menuList.svg";
+import data from "../../data/data.json";
 
 const Category = () => {
   return (
-    <div className="category container">
-      <h2>Kategoriler</h2>
-      <section></section>
-    </div>
+    <nav className="category container">
+      <div className="category__title">
+        <img src={menuList} alt="" className="category__title__icon" />
+        <p className="category__title__text">Kategoriler</p>
+      </div>
+      <section className="category__detail">
+        {data &&
+          data.categories.map((category) => {
+            return (
+              <button key={category.id} className="category__detail__name">
+                {category.name}
+              </button>
+            );
+          })}
+      </section>
+    </nav>
   );
 };
 
