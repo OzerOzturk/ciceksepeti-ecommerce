85253022ca8dbfe867417b4a7016ccaafcbe2bb4
OzerOzturk <ozerozturk1636@gmail.com>
2022-10-08 11:36:01 +0300

    feat(Title.jsx): add Title component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
+import Title from "./components/Title/Title";
 import "./scss/main.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Title />
     </div>
   );
 }
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1 className="title__text">ÇiçekSepeti</h1>
+    </div>
+  );
+};
+
+export default Title;
