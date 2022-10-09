52ce4969ea15a0a0ec03bbbd1c97fed008d79f3f
OzerOzturk <ozerozturk1636@gmail.com>
2022-10-09 12:48:57 +0300

    feat(Footer.jsx): add bottom side of Footer component.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,12 @@ import phonePack from "../../assets/images/phone-pack.png";
 import qrCode from "../../assets/images/qr-code.png";
 import playStore from "../../assets/icons/playStore.svg";
 import appStore from "../../assets/icons/appStore.svg";
+import logo from "../../assets/icons/logo.svg";
+import facebook from "../../assets/icons/facebook.svg";
+import twitter from "../../assets/icons/twitter.svg";
+import instagram from "../../assets/icons/instagram.svg";
+import youtube from "../../assets/icons/youtube.svg";
+import blog from "../../assets/icons/blog.svg";
 
 const Footer = () => {
   return (
@@ -29,10 +35,85 @@ const Footer = () => {
         </div>
       </section>
       <section className="footer__bottom">
-        <div>
-          <h1>hellooo</h1>
+        <div className="footer__bottom__left">
+          <img src={logo} alt="ciceksepeti__logo" />
+          <div className="footer__bottom__left__social">
+            <img src={facebook} alt="facebook__icon" />
+            <img src={twitter} alt="twitter__icon" />
+            <img src={instagram} alt="instagram__icon" />
+            <img src={youtube} alt="youtube__icon" />
+            <img src={blog} alt="ciceksepeti__blog__icon" />
+          </div>
+          <p className="footer__bottom__left__text">
+            CicekSepeti.com olarak kişisel verilerinizin gizliliğini
+            önemsiyoruz. 6698 sayılı Kişisel Verilerin Korunması Kanunu
+            kapsamında oluşturduğumuz aydınlatma metnine&nbsp;
+            <span>buradan</span>&nbsp;ulaşabilirsiniz.
+          </p>
+        </div>
+        <hr />
+        <div className="footer__bottom__right">
+          <div className="footer__bottom__right__subheading">
+            <h4>Faydalı Bilgiler</h4>
+            <ul>
+              <li>Çiçek Bakımı</li>
+              <li>Çiçek Eşliğinde Notlar</li>
+              <li>Çiçek Anlamları</li>
+              <li>Özel Günler</li>
+              <li>Mevsimlere Göre Çiçekler</li>
+              <li>BonnyFood Saklama Koşulları</li>
+              <li>Site Haritası</li>
+            </ul>
+          </div>
+          <div className="footer__bottom__right__subheading">
+            <h4>Kurumsal</h4>
+            <ul>
+              <li>Hakkımızda</li>
+              <li>Kariyer</li>
+              <li>ÇiçekSepeti’nde Satış Yap</li>
+              <li>Kurumsal Müşterilerimiz</li>
+              <li>Reklamlarımız</li>
+              <li>Basında Biz</li>
+              <li>Kampanyalar</li>
+              <li>Vizyonumuz</li>
+            </ul>
+          </div>
+          <div className="footer__bottom__right__subheading">
+            <h4>İletişim</h4>
+            <ul>
+              <li>Bize Ulaşın</li>
+              <li>Sıkça Sorulan Sorular</li>
+            </ul>
+          </div>
+          <div className="footer__bottom__right__subheading">
+            <h4>Gizlilik Sözleşmesi</h4>
+            <ul>
+              <li>Mesafeli Satış Sözleşmesi</li>
+              <li>Bilgi Toplumu Hizmetleri</li>
+              <li>Gizlilik Sözleşmesi</li>
+              <li>Ödeme Seçenekleri</li>
+              <li>Hesap Bilgileri</li>
+            </ul>
+          </div>
         </div>
       </section>
+      <summary>
+        Türkiyenin en büyük online çiçek sitesi ÇiçekSepeti ile sevdiklerinizi
+        mutlu etmek çok kolay! Çiçek göndermenin ve “Mutlu etmenin adresi” Çiçek
+        Sepeti ile sevdiklerinize özel günlerde online çiçek gönderebilirsiniz.
+        Geniş çiçekçi ağı sayesinde çiçek siparişleriniz Türkiye’nin dört bir
+        yanına hızlı ve sorunsuz şekilde gönderilir. Taze çiçeklerle hazırlanan
+        mis kokulu şık çiçek aranjmanları arasından beğendiğiniz ürünü seçerek,
+        hızlı bir şekilde online sipariş verebilirsiniz. Sevdiklerinizin doğum
+        günü, yıldönümü gibi mutlu günlerinde onların sevincine ortak olmak için
+        yapmanız gereken sadece birkaç tıkla sipariş vermek. Sevgililer Günü,
+        Kadınlar Günü, Anneler Günü gibi özel günlerde de çiçek, hediye ve
+        lezzetli bonnyFood ürünleriyle sevdiklerinizi mutlu edebilirsiniz. Çünkü
+        mutlu etmenin adresi; ÇiçekSepeti.
+      </summary>
+      <p className="footer__copyright">
+        Copyright © 2022 Çiçek Sepeti İnternet Hizmetleri A.Ş.
+      </p>
     </footer>
   );
 };
