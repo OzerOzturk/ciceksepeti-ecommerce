78d577a6bfd6cfd61d44132e8ca90cb2bdf83100
OzerOzturk <ozerozturk1636@gmail.com>
2022-10-10 03:18:51 +0300

    revise: remove unnecessary files.

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -49,34 +49,3 @@ const ProductList = () => {
 };
 
 export default ProductList;
-
-// YEDEK RETURN
-/*  return (
-    <div className="productList container">
-      <div className="productList__heading">
-        <img
-          src={leaf}
-          alt="leaf_icon"
-          className="productList__heading__image"
-        />
-        <h2 className="productList__heading__text">Tüm Kategoriler</h2>
-      </div>
-
-      <main className="productList__card">
-        {data &&
-          data.products.map((product) => {
-            return (
-              <ProductCard
-                key={product.id}
-                id={product.id}
-                name={product.name}
-                delivery={product.freeDelivery}
-                price={product.price}
-                image={product.image}
-                category={product.category}
-              />
-            );
-          })}
-      </main>
-    </div>
-  ); */
